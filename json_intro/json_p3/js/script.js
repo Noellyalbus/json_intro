1f2b90347dafca5266b70b53b8545bf8cf848e36
Noelly Albus <115089251+Noellyalbus@users.noreply.github.com>
2023-06-02 15:34:07 -0300

    Add files via upload

---
+++ json_intro/json_p3/js/script.js
@@ -0,0 +1,59 @@
+const nomeArquivo = 'json/dat.json';
+
+function quiz(){
+    function extrairDados() {
+        fetch(nomeArquivo)
+            .then(response => response.json())
+            .then(data => {
+                const perguntas = data.perguntas;
+                for (k = 0; k < perguntas.length; k++){
+                    const perguntaSpan = document.createElement('h2');
+                    perguntaSpan.innerHTML = `Pergunta: ${perguntas[k].questao}`;
+                    document.body.appendChild(perguntaSpan);
+
+                    const resposta = perguntas[k].resposta;
+                    
+                    var itemCorreto;
+                    switch(resposta){
+                        case 'a':
+                            itemCorreto = 0;
+                        break;
+                        case 'b':
+                            itemCorreto = 1;
+                        break; 
+                        case 'c':
+                            itemCorreto = 2;
+                        break;
+                        case 'd':
+                            itemCorreto = 3;
+                        break;    
+                        case 'e':
+                            itemCorreto = 4;
+                        break;    
+                    }    
+                    
+                    const opcoes = perguntas[k].opcoes;
+
+                    for (j= 0; j < opcoes.length; j++ ){
+                        const opcao = document.createElement('p');
+
+                        opcao.innerHTML = opcoes[j];
+
+                        if (j == itemCorreto){
+                            opcao.style.color = "red";
+                        }
+                        
+                        document.body.appendChild(opcao);
+                    }
+
+
+                }
+                
+            })
+            .catch(error => {
+                console.error('Erro ao ler o arquivo JSON:', error);
+            });
+    } 
+    
+    extrairDados();
+}
